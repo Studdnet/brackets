8b0a48fbdfece2a5b8cbc0978328703541a23861
Studdnet <St.codding@gmail.com>
2019-03-22 11:10:23 +0300

    docs: changed algorithm find brackets

--- src/index.js
+++ src/index.js
@@ -1,54 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-    //var arr=['([{}])'];
-    //var exampl = [['(', ')'], ['[', ']'], ['{', '}']];
     let arrayString = str.split('');  
-    let check;
-    for (let i=0; i<bracketsConfig.length; i++) {    
-        let countX=0, countY=0;
-        for (let x = 0; x <= arrayString.length; x++) {
-            if (arrayString[x] == bracketsConfig[i][0]) {
-                countX++;
-            }
-        }
-        for (let y = 0; y <= arrayString.length; y++) {
-            if (arrayString[y] == bracketsConfig[i][1]) {
-                countY++;
-            }
-        }
-        if (countX != countY){
-            check=false;
-            break;
-        } else {
-            check=true;
-        }
-    }
-    if (check==true) {
-        for (let f=0; f<=bracketsConfig.length+1; f++) {
-            for (let a=0; a<=bracketsConfig.length; a++) {
-                for (let b = 0; b <= arrayString.length; b++) {
-                    for (let j = 0; j <= arrayString.length; j++) {
-                        if (arrayString[j] == bracketsConfig[a][0] && arrayString[j+1] == bracketsConfig[a][1]) {
-                            delete arrayString[j];   
-                            delete arrayString[j+1]; 
+    var superLen  =  50;
+            for (let i=0; i<superLen; i++) {
+                for (let j=0; j<bracketsConfig.length; j++) {
+                    for (let k = 0; k < arrayString.length; k++) {
+                        if (arrayString[k] == bracketsConfig[j][0] && arrayString[k+1] == bracketsConfig[j][1]) {
+                            delete arrayString[k];   
+                            delete arrayString[k+1]; 
                             arrayString = arrayString.filter(element => element !== undefined);
+                            j = 0;
                         }
                     }
-                    if (arrayString.length == 0) {
-                        return true;
-                    }
-                }
-                if (a==(bracketsConfig.length-1)) {
-                    break;
                 }
             }
-        }
         if (arrayString.length != 0) {
             return false;
-        } else if (arrayString.length == 0) {
-            return true; 
+        } else {
+            return true;
         }
-    } else {
-        return false;
-    }    
-}
+} 
+
